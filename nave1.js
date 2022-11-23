2cfc4865c8c9b167da8083e5ec50f42eac507294
Paradis <Lucasezequiel120202@gmail.com>
2022-11-23 12:28:27 -0300

    update

--- nave1.js
+++ nave1.js
@@ -5,7 +5,7 @@ var nave = {
     ang: 0,
     vang: 0,
     aang: 0,
-    masa: 3
+    masa: 3, vivo: true
 }
 var multiplicadorCohete = 300
 var multiplicadorDisparo = 7000
@@ -13,18 +13,60 @@ var torqueGiro = 3000
 var multiplicadorFriccion = 0
 var multiplicadorFriccionGiro = 1500
 var bullets = [];
+var meters = [];
+var meterSpawnTime = 60;
+var meterSpawnTimeCounter = 0;
 
+const H = 900; const W = 900
 function setup() {
-    createCanvas(900, 900);
+    createCanvas(H, W);
 }
 let fuerzaCohete = { x: 0, y: 0 }
 
 function draw() {
     background(220);
     var dt = deltaTime / 1000
+    
+
+    meterSpawnTimeCounter++
+    if (meterSpawnTime == meterSpawnTimeCounter && nave.vivo) {
+        console.log("3 segundos")
+        meterSpawnTimeCounter = 0
+
+        let r = getRandomInt(4)
+        console.log(r)
+        switch (r) {
+            case 0:
+                // left wall
+                meters.push(new Meteorito(nave.x, nave.y, 0, random(H)))
+                break;
+            case 1:
+                // right wall
+                meters.push(new Meteorito(nave.x, nave.y, W, random(H)))
+                break;
+            case 2:
+                // top wall
+                meters.push(new Meteorito(nave.x, nave.y, random(W), 0))
+                break;
+            case 3:
+                // bottom wall
+                meters.push(new Meteorito(nave.x, nave.y, random(W), H))
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    if (nave.x < 0 || nave.x > W || nave.y < 0 || nave.y > H) {
+        text("PERDISTE r para reinicar", 450, 450)
+        nave.vivo = false
+        meterSpawnTimeCounter = 0
+    }
 
     // Si tengo presionada la tecla de arriba...
-    if (keyIsDown(UP_ARROW)) {
+    if (keyIsDown(UP_ARROW) && nave.vivo) {
         // Obtengo la dirección de la nave con trigonometría.
         // Esto me da un vector de longitud 1
         var direccionNave = {
@@ -43,7 +85,7 @@ function draw() {
         // y lo aplico como fuerza
         aplicarFuerza(nave, fuerzaCohete.x, fuerzaCohete.y)
     }
-    if (keyIsDown(DOWN_ARROW)) {
+    if (keyIsDown(DOWN_ARROW) && nave.vivo) {
         // Obtengo la dirección de la nave con trigonometría.
         // Esto me da un vector de longitud 1
         var direccionNave = {
@@ -70,7 +112,7 @@ function draw() {
     data.push(["vy", nave.vy])
     data.push(["ax", nave.ax])
     data.push(["ay", nave.ay])
-    
+
     data.push(["masa", nave.masa])
 
 
@@ -82,19 +124,15 @@ function draw() {
     aplicarFuerza(nave, fuerzaFriccion.x, fuerzaFriccion.y)
 
     // Si estoy apretando izquierda o derecha, aplico un torque
-    if (keyIsDown(RIGHT_ARROW)) {
+    if (keyIsDown(RIGHT_ARROW) && nave.vivo) {
         aplicarTorque(nave, torqueGiro)
     }
-    if (keyIsDown(LEFT_ARROW)) {
+    if (keyIsDown(LEFT_ARROW) && nave.vivo) {
         aplicarTorque(nave, -torqueGiro)
     }
 
-    // Aplico un torque proporcional y opuesto a la velocidad angular de la nave
-    //var torqueFriccion = -nave.vang * multiplicadorFriccionGiro
-    //aplicarTorque(nave, torqueFriccion)
-
     // Calculo las ecuaciones de movimiento
-    calcularFisicas(nave, dt)
+    if (nave.vivo) calcularFisicas(nave, dt)
     data.push(["ang", nave.ang])
     data.push(["vang", nave.vang])
     data.push(["aang", nave.aang])
@@ -118,21 +156,37 @@ function draw() {
     resetMatrix() // vuelvo la hoja a su lugar
 
     for (let i = 0; i < bullets.length; ++i) {
-        bullets[i].toMouse();
+        if (nave.vivo) bullets[i].toMouse();
         bullets[i].show();
+    }
+
+    for (let i = 0; i < meters.length; ++i) {
+        if (nave.vivo) meters[i].move();
+        meters[i].show();
     }
 }
 
 function keyPressed() {
-    if (keyCode == 32) {
+    if (keyCode == 32 && nave.vivo) {
         bullets.push(new Bullet(cos(nave.ang) + nave.x, sin(nave.ang) + nave.y, nave.x, nave.y))
         aplicarFuerza(nave, -(cos(nave.ang) * multiplicadorDisparo), -(sin(nave.ang) * multiplicadorDisparo))
+    }
+    if (keyCode == 82 && !nave.vivo) {
+        nave.x = 200
+        nave.y = 200
+        nave.vx = 0
+        nave.vy = 0
+        nave.ang = 0
+        nave.vang = 0
+        nave.vivo = true
+        bullets = []
+        meters = []
     }
 
 }
 
 function Bullet(X, Y, PX, PY) {
-    this.speed = 2;
+    this.speed = 5;
     this.x = PX;
     this.y = PY;
     this.dir = createVector(X - PX, Y - PY).normalize()
@@ -147,6 +201,25 @@ function Bullet(X, Y, PX, PY) {
     }
 }
 
+function Meteorito(X, Y, PX, PY) {
+    this.speed = 3;
+    this.x = PX;
+    this.y = PY;
+    this.dir = createVector(X - PX, Y - PY).normalize();
+    this.r = 30;
+    this.show = function () {
+        fill("brown")
+        circle(this.x, this.y, this.r)
+        fill("black")
+
+    }
+    this.move = function () {
+        this.x += this.dir.x * this.speed;
+        this.y += this.dir.y * this.speed;
+    }
+
+}
+
 function calcularFisicas(cuerpo, dt) {
     // a -> v -> x
     cuerpo.vx += cuerpo.ax * dt
@@ -159,7 +232,7 @@ function calcularFisicas(cuerpo, dt) {
     cuerpo.ang += cuerpo.vang * dt
 
     // reinicio todas las aceleraciones
-    
+
     cuerpo.ax = 0
     cuerpo.ay = 0
 }
@@ -182,3 +255,8 @@ function mostrarText(data) {
         text(nombre + ": " + valor.toFixed(3), 10, (i + 1) * 20)
     }
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
